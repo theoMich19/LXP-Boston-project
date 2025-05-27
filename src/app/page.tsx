7d36395f934aae1ec910d78c76a13a5eb1c7d958
theoMich <michellontheo.pro@gmail.com>
2025-05-27 00:43:55 -0400

    feat(home-search): start implement search & api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,16 @@
-"use client";
-
-import Image from "next/image";
-import { toastUtils, showApiError, showSaveSuccess } from '@/utils/toast';
-
-const testToast = () => {
-
-  // Utilisation basique
-  toastUtils.info("Information importante");
-  toastUtils.success("Opération réussie !");
-  toastUtils.warn("Attention à ceci");
-  toastUtils.error("Une erreur est survenue");
-
-  // Toast de chargement
-  const loadingToast = toastUtils.loading("Sauvegarde en cours...");
-  // Plus tard...
-  toastUtils.updateLoadingToSuccess(loadingToast, "Sauvegardé avec succès !");
-
-  // Gestion automatique des promesses
-  toastUtils.promise(
-    fetch('/api/data'),
-    {
-      pending: 'Chargement des données...',
-      success: 'Données chargées !',
-      error: 'Erreur lors du chargement'
-    }
-  );
-
-  // Fonctions utilitaires
-  showApiError("truc muche erreur");
-  showSaveSuccess("utilisateur");
-}
+import AdressSearchInput from "@/components/search/search";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
+
+
   return (
-    <div>
-      <button className="bg-blue-500 text-white p-2 rounded-md" onClick={testToast}>Test Toast</button>
-    </div>
-  );
+    <Card className={`p-6 max-w-2xl mx-auto`}>
+      <Input />
+      <AdressSearchInput />
+      <Button>Rechercher</Button>
+    </Card>
+  )
 }
