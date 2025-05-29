8ccfd262f0d73fa94e76367f3bde848aea748902
theoMich <michellontheo.pro@gmail.com>
2025-05-28 20:18:30 -0400

    update(layout): update layout app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <span>cocou</span>
         {children}
         <ToastContainer aria-label={undefined} />
       </body>
